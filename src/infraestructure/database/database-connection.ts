3d74437ee6e84e47f0aa9e9f9735d8d2865a93eb
Matheus <matheusbastosandrade@gmail.com>
2024-05-20 20:05:58 -0300

    fix: trocando postgres.js por pg-promise

--- src/infraestructure/database/database-connection.ts
+++ src/infraestructure/database/database-connection.ts
@@ -1,26 +1,25 @@
-import postgres from "postgres";
+import pgp from "pg-promise";
+import pg from "pg-promise/typescript/pg-subset";
 import { configDotenv } from "dotenv";
 
 const env = configDotenv();
 
 type DatabaseConnection = {
-  query<Entity extends object>(queryString: string): Promise<postgres.RowList<Entity[]>>;
+  query(queryString: string, params: unknown[]): Promise<any>;
 };
 
-class PostgresAdapter implements DatabaseConnection {
-  public connection: postgres.Sql;
+class PgpAdapter implements DatabaseConnection {
+  private connection: pgp.IDatabase<{}, pg.IClient>;
 
   constructor() {
-    this.connection = postgres(process.env.DATABASE_URL as string);
+    this.connection = pgp()(process.env.DATABASE_URL as string);
   }
 
-  public async query<Entity extends object>(
-    queryString: string
-  ): Promise<postgres.RowList<Entity[]>> {
-    return await this.connection<Entity[]>/*sql*/ `
-      ${queryString}
-    `;
+  public async query(queryString: string, params: unknown[]): Promise<any> {
+    const query = await this.connection.query(queryString, params);
+
+    return query;
   }
 }
 
-export { DatabaseConnection, PostgresAdapter };
+export { DatabaseConnection, PgpAdapter };

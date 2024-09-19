03edd0357abdb513cf5add2b4d9fe304a9bed020
Matheus <matheusbastosandrade@gmail.com>
2024-09-19 16:32:03 -0300

    feat: forma alternativa para build

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,42 @@
-import { PixRoutes } from "./routes/pix-routes";
-import { FoodRoutes } from "./routes/food-routes";
-import { PgpAdapter } from "./infraestructure/database/database-connection";
-import { UserRoutes } from "./routes/user-routes";
-import { CardRoutes } from "./routes/card-routes";
-import { JwtAdapter } from "./infraestructure/token/token";
-import { OrderRoutes } from "./routes/order-routes";
-import { BcryptAdapter } from "./infraestructure/cryptography/cryptography";
-import { ExpressAdapter } from "./infraestructure/http/http-server";
-import { EmployeeRoutes } from "./routes/employee-routes";
-import { DeliveryRoutes } from "./routes/delivery-routes";
-import { OrderFoodRoutes } from "./routes/order-food-routes";
-import { DeliverymanRoutes } from "./routes/deliveryman-routes";
+// import { PixRoutes } from "./routes/pix-routes";
+// import { FoodRoutes } from "./routes/food-routes";
+// import { PgpAdapter } from "./infraestructure/database/database-connection";
+// import { UserRoutes } from "./routes/user-routes";
+// import { CardRoutes } from "./routes/card-routes";
+// import { JwtAdapter } from "./infraestructure/token/token";
+// import { OrderRoutes } from "./routes/order-routes";
+// import { BcryptAdapter } from "./infraestructure/cryptography/cryptography";
+// import { ExpressAdapter } from "./infraestructure/http/http-server";
+// import { EmployeeRoutes } from "./routes/employee-routes";
+// import { DeliveryRoutes } from "./routes/delivery-routes";
+// import { OrderFoodRoutes } from "./routes/order-food-routes";
+// import { DeliverymanRoutes } from "./routes/deliveryman-routes";
 
-// adapters/banco de dados
-const httpServer = new ExpressAdapter();
-const connection = new PgpAdapter();
-const cryptography = new BcryptAdapter();
-const token = new JwtAdapter();
+// // adapters/banco de dados
+// const httpServer = new ExpressAdapter();
+// const connection = new PgpAdapter();
+// const cryptography = new BcryptAdapter();
+// const token = new JwtAdapter();
 
-// routes
-new FoodRoutes(connection, httpServer).routes();
-new EmployeeRoutes(connection, httpServer, cryptography, token).routes();
-new DeliverymanRoutes(connection, httpServer, cryptography, token).routes();
-new UserRoutes(connection, httpServer, cryptography, token).routes();
-new CardRoutes(connection, httpServer).routes();
-new PixRoutes(connection, httpServer).routes();
-new DeliveryRoutes(connection, httpServer).routes();
-new OrderRoutes(connection, httpServer).routes();
-new OrderFoodRoutes(connection, httpServer).routes();
+// // routes
+// new FoodRoutes(connection, httpServer).routes();
+// new EmployeeRoutes(connection, httpServer, cryptography, token).routes();
+// new DeliverymanRoutes(connection, httpServer, cryptography, token).routes();
+// new UserRoutes(connection, httpServer, cryptography, token).routes();
+// new CardRoutes(connection, httpServer).routes();
+// new PixRoutes(connection, httpServer).routes();
+// new DeliveryRoutes(connection, httpServer).routes();
+// new OrderRoutes(connection, httpServer).routes();
+// new OrderFoodRoutes(connection, httpServer).routes();
 
-httpServer.listen(3000);
+// httpServer.listen(3000);
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("agora está funcionando");
+});
+
+app.listen(3000);
